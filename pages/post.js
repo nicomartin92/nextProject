312db15b1b2888e987bd6c8436de482973650282
Nicolas Martin <nicolas27martin@gmail.com>
2019-09-12 12:03:05 +0200

    add dynamic routers

---
+++ pages/post.js
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router'
+import Layout from '../components/MyLayout.js'
+
+const Page = () => {
+  const router = useRouter();
+
+  console.warn(router);
+  
+
+  return (
+    <Layout>
+      <h1>{router.query.title}</h1>
+      <p>This is the blog post content.</p>
+    </Layout>
+  )
+}
+
+export default Page;
