4f2fe0f374f2cf0c14f707affb29aed25535dad0
Nicolas Martin <nicolas27martin@gmail.com>
2019-10-28 11:14:52 +0100

    add selectors on redux

--- store/selectors/selectors.js
+++ store/selectors/selectors.js
@@ -1,4 +1,16 @@
-const getALLCars = store =>
-    store.cars.map(car => car);
+export const getALLCars = store => {
+    return store.carR.cars.filter(car => car);
+}
 
-export default getALLCars
+export const getItalianCars = store => {
+    return store.carR.cars.filter(car => car.country === 'it');
+}
+
+export const getFrenchCars = store => {
+    return store.carR.cars.filter(car => car.country === 'fr');
+}
+
+export const getGermanCars = store => {
+    return store.carR.cars.filter(car => car.country === 'de');
+}
+    
