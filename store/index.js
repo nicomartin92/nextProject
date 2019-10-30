52545320a48ffac5a82beef57b839af11de63a6c
Nicolas Martin <nicolas27martin@gmail.com>
2019-10-30 13:57:18 +0100

    add redux persist

--- store/index.js
+++ store/index.js
@@ -1,6 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
+// REDUX PERSIST
+import { persistStore } from 'redux-persist';
+
 // Import Selectors
 import selectors from './selectors/selectors';
 
@@ -13,9 +16,38 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 /* eslint-disable no-underscore-dangle */
 export const initStore = (initialState = {}) => {
-  return createStore(
-    rootReducer, 
-    initialState,
-    composeWithDevTools(applyMiddleware(thunk)));
+  let store;
+  const isClient = typeof window !== 'undefined';
+
+  // only if client browser
+  if (isClient) {
+    const { persistReducer } = require('redux-persist');
+    const storage = require('redux-persist/lib/storage').default;
+    const persistConfig = {
+      key: 'root',
+      storage
+    };
+
+    store = createStore(
+      persistReducer(persistConfig, rootReducer),
+      initialState,
+      composeWithDevTools(applyMiddleware(thunk))
+    );
+
+    store.__PERSISTOR = persistStore(store);
+
+    return store;
+  } else {
+    /* 
+     * If persist is not need remove if isClient 
+     * Lignes 5, 19 to 40
+     */
+    return createStore(
+      rootReducer,
+      initialState,
+      composeWithDevTools(applyMiddleware(thunk)));
+  }
+
+
 };
 /* eslint-enable */
