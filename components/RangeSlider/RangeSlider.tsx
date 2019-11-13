46401e3c043db32a66fa5bd419f92a152b6aa5ba
Nicolas Martin <nicolas27martin@gmail.com>
2019-11-13 15:55:35 +0100

    add correct position cursor

--- components/RangeSlider/RangeSlider.tsx
+++ components/RangeSlider/RangeSlider.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import PubSub from 'pubsub-js';
 
 import './RangeSlider.scss'
 
 const RangeSlider = (props: any) => {
-    const [value, setValue] = React.useState(1); // from 50 - 220 €
+    const [value, setValue] = React.useState(3); // from 50 - 220 €
     const Range = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220];
 
     const handleChange = (e: any) => {
         e.preventDefault();
-        setValue(e.target.value);
-        const searchValue = Range[e.target.value];
+        const indexValue = e.target.value;
+
+        setValue(indexValue);
+        const searchValue = Range[indexValue];
+
+        console.warn(indexValue, searchValue);
 
         props.setCarsDataJsonFromState(searchValue)
     }
@@ -22,10 +25,10 @@ const RangeSlider = (props: any) => {
     const genSlideStyle = (value: any) => {
         return {
             point: {
-                left: `calc(${value * 5.5}% - ${10}px)`,
+                left: `calc(${value * 5.58}% - ${15}px)`,
             },
             range: {
-                width: `${value * 5.5}%`,
+                width: `${value * 5.58}%`,
             },
         };
     };
@@ -33,21 +36,25 @@ const RangeSlider = (props: any) => {
     const slideStyle = genSlideStyle(value);
 
     return (
-        <div className="rangeSlider">
-            {bulletPoints}
-            <span className="rangeSlider__value" style={slideStyle.range} />
-            <span className="circle" style={slideStyle.point} />
-            <input
-                className="rangeSlider__slide"
-                name="range"
-                type="range"
-                min="0"
-                max="18"
-                value={value}
-                step="1"
-                onChange={handleChange}
-            />
+        <div>
+            <h2 className="center">Filtrer par prix:</h2>
+            <div className="rangeSlider">
+                {bulletPoints}
+                <span className="rangeSlider__value" style={slideStyle.range} />
+                <span className="circle" style={slideStyle.point} />
+                <input
+                    className="rangeSlider__slide"
+                    name="range"
+                    type="range"
+                    min="0"
+                    max="17"
+                    value={value}
+                    step="1"
+                    onChange={handleChange}
+                />
+            </div>
         </div>
+
     )
 }
 
