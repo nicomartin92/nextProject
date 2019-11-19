41072c5ed9d1ff60a7f601a1768b85aaa8253919
Nicolas Martin <nicolas27martin@gmail.com>
2019-11-19 21:51:12 +0100

    add percentage calculation

--- components/Survey/Survey.tsx
+++ components/Survey/Survey.tsx
@@ -11,7 +11,7 @@ const Survey = (props: any) => {
     const [successMessage, setSuccessMessage] = React.useState(false);
     const [choice, setChoice] = React.useState("");
 
-    const checkForm = (e:any) => {
+    const checkForm = (e: any) => {
         if (document.querySelector('.inputRadio:checked')) {
             const db = firebase.firestore();
             db.collection("survey").add({
@@ -26,7 +26,7 @@ const Survey = (props: any) => {
         e.preventDefault();
     }
 
-    const checkRadio = (e:any) => {
+    const checkRadio = (e: any) => {
         document.querySelectorAll('.inputRadio').forEach((elem) => {
             elem.classList.remove('checked');
         })
@@ -35,13 +35,13 @@ const Survey = (props: any) => {
         setChoice(e.target.value);
     }
 
-    const displayRadio = props.item.questions.map((question:any) => {
+    const displayRadio = props.item.questions.map((question: any) => {
         return (
             <div key={`model-${Math.random()}`}>
                 <input type="radio"
-                    id={question.id} 
-                    onChange={checkRadio} 
-                    className="inputRadio" 
+                    id={question.id}
+                    onChange={checkRadio}
+                    className="inputRadio"
                     name="radio-group"
                     value={`model-${question.id}`} />
                 <label htmlFor={question.id}>{question.model}</label>
@@ -49,14 +49,52 @@ const Survey = (props: any) => {
         )
     })
 
+    const getData = () => {
+        const [brand, setBrand] = React.useState([]);
+
+        React.useEffect(() => {
+            firebase
+                .firestore()
+                .collection('survey')
+                .onSnapshot((snapshot) => {
+                    const newDatas = snapshot.docs.map((doc) => ({
+                        id: doc.id,
+                        ...doc.data()
+                    }));
+
+                    setBrand(newDatas);
+                })
+        }, []);
+
+        return brand;
+    }
+    const surveyDatas = getData();
+    
+    const percentages = () => {
+        const ratioPercentage = 100/surveyDatas.length;
+        const model1Length = surveyDatas.filter(val => val.choice === 'model-1').length
+        const model1 = model1Length * ratioPercentage;
+        const model2 = 100 - model1;
+        const newArr = [];
+        newArr.push(model1)
+        newArr.push(model2);
+        return newArr;
+    }
+
+    const percentageValues = percentages();
+
     return (
         <div className="survey">
             <h3 className="survey__label">{props.item.label}</h3>
-
+            {percentageValues.map((survey) =>
+                <div className="list" key={survey.id}>
+                    {survey} %
+                </div>
+            )}
             <div className="progress">
 
             </div>
-            
+
             <p className={errorMessage ? "errorMessage" : "hidden"}>Veuillez sélectionner un champs avant !</p>
             <p className={successMessage ? "successMessage" : "hidden"}>Votre réponse a bien été envoyée !</p>
             <form action="#" method="post" onSubmit={checkForm}>
