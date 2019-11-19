f005926a30095800e7160c6a14b0ffb2d498a80e
Nicolas Martin <nicolas27martin@gmail.com>
2019-11-19 17:47:41 +0100

    add new data in firestore

--- components/Ratings/Ratings.tsx
+++ components/Ratings/Ratings.tsx
@@ -3,12 +3,21 @@ import React from 'react';
 // SCSS
 import './Ratings.scss';
 
+// Get Firestore
+import firebase from '../../lib/db';
+
 const Ratings = (props: any) => {
     const [errorMessage, setErrorMessage] = React.useState(false);
     const [successMessage, setSuccessMessage] = React.useState(false);
+    const [choice, setChoice] = React.useState("");
 
-    const checkForm = (e) => {
-        if(document.querySelector('.inputRadio:checked')) {
+    const checkForm = (e:any) => {
+        if (document.querySelector('.inputRadio:checked')) {
+            const db = firebase.firestore();
+            db.collection("ratings").add({
+                choice: choice
+            });
+
             setSuccessMessage(true);
         } else {
             setErrorMessage(true);
@@ -17,18 +26,24 @@ const Ratings = (props: any) => {
         e.preventDefault();
     }
 
-    const checkRadio = (e) => {
+    const checkRadio = (e:any) => {
         document.querySelectorAll('.inputRadio').forEach((elem) => {
             elem.classList.remove('checked');
         })
 
         e.target.classList.add('checked', 'checked');
+        setChoice(e.target.value);
     }
 
-    const displayRadio = props.item.questions.map((question) => {
+    const displayRadio = props.item.questions.map((question:any) => {
         return (
             <div key={`model-${Math.random()}`}>
-                <input type="radio" id={question.id} onChange={checkRadio} className="inputRadio" name="radio-group" />
+                <input type="radio"
+                    id={question.id} 
+                    onChange={checkRadio} 
+                    className="inputRadio" 
+                    name="radio-group"
+                    value={`model-${question.id}`} />
                 <label htmlFor={question.id}>{question.model}</label>
             </div>
         )
@@ -37,8 +52,8 @@ const Ratings = (props: any) => {
     return (
         <div className="ratings">
             <h3 className="ratings__label">{props.item.label}</h3>
-            <p className={ errorMessage ? "errorMessage": "hidden"}>Veuillez sélectionner un champs avant !</p>
-            <p className={ successMessage ? "successMessage": "hidden"}>Veuillez sélectionner un champs avant !</p>
+            <p className={errorMessage ? "errorMessage" : "hidden"}>Veuillez sélectionner un champs avant !</p>
+            <p className={successMessage ? "successMessage" : "hidden"}>Votre réponse a bien été envoyée !</p>
             <form action="#" method="post" onSubmit={checkForm}>
                 {displayRadio}
                 <button className="ratings__submit button" aria-label={props.item.submit}>{props.item.submit}</button>
