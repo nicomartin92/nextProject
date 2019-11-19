369b8cd021c89a88764fd3ec0c9761c4b20505b2
Nicolas Martin <nicolas27martin@gmail.com>
2019-11-19 22:08:53 +0100

    add percentage display values

--- components/Survey/Survey.tsx
+++ components/Survey/Survey.tsx
@@ -18,9 +18,11 @@ const Survey = (props: any) => {
                 choice: choice
             });
 
+            setErrorMessage(false);
             setSuccessMessage(true);
         } else {
             setErrorMessage(true);
+            setSuccessMessage(false);
         }
 
         e.preventDefault();
@@ -33,6 +35,7 @@ const Survey = (props: any) => {
 
         e.target.classList.add('checked', 'checked');
         setChoice(e.target.value);
+        setErrorMessage(false);
     }
 
     const displayRadio = props.item.questions.map((question: any) => {
@@ -76,8 +79,8 @@ const Survey = (props: any) => {
         const model1 = model1Length * ratioPercentage;
         const model2 = 100 - model1;
         const newArr = [];
-        newArr.push(model1)
-        newArr.push(model2);
+        newArr.push({model: props.item.questions[0].model, percentage: model1})
+        newArr.push({model: props.item.questions[1].model, percentage: model2});
         return newArr;
     }
 
@@ -86,17 +89,19 @@ const Survey = (props: any) => {
     return (
         <div className="survey">
             <h3 className="survey__label">{props.item.label}</h3>
+        
+            <p className={errorMessage ? "errorMessage" : "hidden"}>Veuillez sélectionner un champs avant !</p>
+            <p className={successMessage ? "successMessage" : "hidden"}>Votre réponse a bien été envoyée !</p>
+            
             {percentageValues.map((survey) =>
                 <div className="list" key={survey.id}>
-                    {survey} %
+                   {survey.model} {Math.round(survey.percentage)} %
                 </div>
             )}
             <div className="progress">
 
             </div>
 
-            <p className={errorMessage ? "errorMessage" : "hidden"}>Veuillez sélectionner un champs avant !</p>
-            <p className={successMessage ? "successMessage" : "hidden"}>Votre réponse a bien été envoyée !</p>
             <form action="#" method="post" onSubmit={checkForm}>
                 {displayRadio}
                 <button className="survey__submit button" aria-label={props.item.submit}>{props.item.submit}</button>
