f6d69b0bbe8e47dd91b9e8784d5961e3d9d72665
Nicolas Martin <nicolas27martin@gmail.com>
2020-05-02 16:31:40 +0200

    add new data

--- pages/status.js
+++ pages/status.js
@@ -7,19 +7,19 @@ import Dashboard from '../components/Dashboard/Dashboard'
 import { connect } from 'react-redux';
 
 const Status = (props) => {
-    return (
-      <div>
-        <Head>
-          <title>@FRmodelcars - tableau de bord</title>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
+  return (
+    <div>
+      <Head>
+        <title>@FRmodelcars - tableau de bord</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
 
-        <Layout carItems={props.carR.cars}>
-          <p>This is the Status page</p>
-          <Dashboard items={props.carR.cars} />
-        </Layout>
-      </div>
-    )
+      <Layout carItems={props.carR.cars}>
+        <p>This is the Status page</p>
+        <Dashboard items={props.carR.cars} />
+      </Layout>
+    </div>
+  )
 }
 
 export default connect(
