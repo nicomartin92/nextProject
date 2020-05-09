cfec6c16e360d5e0fc3a959db00de152976facb3
Nicolas Martin <nicolas27martin@gmail.com>
2020-05-09 15:14:53 +0200

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 const withSass = require("@zeit/next-sass");
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+    pwa: {
+        dest: 'public'
+    }
+})
 
 module.exports = withSass();
